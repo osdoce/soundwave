9f7fcb0eb7cf4cb11bdb6cbda680556fee8ce1d6
FabianSaldierna <fsaldierna@banxico.org.mx>
2025-01-21 19:15:31 -0600

    deleted client and src folders

--- soundwave/client/src/main.tsx
+++ soundwave/client/src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.tsx';
+import Board from './pages/Board.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
+import EditTicket from './pages/EditTicket.tsx';
+import CreateTicket from './pages/CreateTicket.tsx';
+import Login from './pages/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Board />
+      }, 
+      {
+        path: '/edit',
+        element: <EditTicket />
+      },
+      {
+        path: '/create',
+        element: <CreateTicket />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+])
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
